57d45b0870b07764a8a50c2ed22957c5287fa518
Owais Khan <owais1566@gmail.com>
2020-05-27 05:42:15 +0000

    updated version

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View } from 'native-base';
 import { Provider } from 'react-redux';
 // import { StyleSheet, Text } from 'react-native';
 import { store } from './store';
-import Navigator from './navigation';
+import MainNavigator from './navigation';
 import { setLocalNotification } from './utils/notifications';
 
 export default class App extends Component {
@@ -16,7 +16,7 @@ export default class App extends Component {
     return (
       <Provider store={store}>
         <View style={{ flex: 1, paddingTop: 22 }}>
-          <Navigator />
+          <MainNavigator />
         </View>
       </Provider>
     )
--- store/middleware/index.js
+++ store/middleware/index.js
@@ -1,28 +1,8 @@
-import { RECEIVE_DECKS, STORE_DECK, STORE_CARD_TO_DECK } from '../actions'
+import thunk from 'redux-thunk'
+import logger from './logger'
+import { applyMiddleware } from 'redux'
 
-function decks(state = {}, action) {
-    switch (action?.type) {
-        case RECEIVE_DECKS:
-            return {
-                ...state,
-                decks: action.decks
-            }
-        case STORE_DECK:
-            return {
-                decks: {
-                    ...state.decks,
-                    [action.deck.title]: action.deck
-                }
-            }
-        case STORE_CARD_TO_DECK:
-            state.decks[action.deckTitle].questions.push(action.card)
-            return {
-                ...state,
-                decks: state.decks
-            }
-        default:
-            return state
-    }
-}
-
-export default decks
+export default applyMiddleware(
+    thunk,
+    logger,
+)
